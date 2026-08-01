395b9dd2d9007608d046047312b215f9d734dfa4
chakalov petkov <petkovrichard8@gmail.com>
2026-08-01 10:58:23 +0100

    Wire user and onboarding providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CitizenLayout } from './components/CitizenLayout'
+import { OnboardingOverlay } from './components/onboarding/OnboardingOverlay'
+import { OnboardingProvider } from './context/OnboardingContext'
 import { ReportsProvider } from './context/ReportsContext'
+import { UserProvider } from './context/UserContext'
 import { MapHomePage } from './pages/MapHome'
 import { ReportDetailsPage } from './pages/ReportDetails'
 import { ReportFlowPage } from './pages/ReportFlow'
@@ -11,18 +14,23 @@ import { DashboardPage } from './pages/Dashboard'
 export default function App() {
   return (
     <ReportsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<CitizenLayout />}>
-            <Route path="/" element={<MapHomePage />} />
-            <Route path="/report" element={<ReportFlowPage />} />
-            <Route path="/my-reports" element={<MyReportsPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/reports/:id" element={<ReportDetailsPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <OnboardingProvider>
+            <Routes>
+              <Route element={<CitizenLayout />}>
+                <Route path="/" element={<MapHomePage />} />
+                <Route path="/report" element={<ReportFlowPage />} />
+                <Route path="/my-reports" element={<MyReportsPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/reports/:id" element={<ReportDetailsPage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+              </Route>
+            </Routes>
+            <OnboardingOverlay />
+          </OnboardingProvider>
+        </BrowserRouter>
+      </UserProvider>
     </ReportsProvider>
   )
 }
